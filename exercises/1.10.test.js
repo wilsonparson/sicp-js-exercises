e2d687547f696d9d979e17b1619986b4890f679a
Wilson Parson <wilson.parson@gmail.com>
2022-05-18 15:37:08 -0600

    1.9 & 1.10

--- exercises/1.10.test.js
+++ exercises/1.10.test.js
@@ -35,9 +35,90 @@ if (false) {
   1024;
 }
 
-// When x is 1, the answer is 2^y
+// From this result we know that when x is 1, the result is 2^y
 
 // A(2, 4);
 if (false) {
   A(2, 4);
+  A(2 - 1, A(2, 4 - 1));
+  A(1, A(2, 3));
+  A(1, A(2 - 1, A(2, 3 - 1)));
+  A(1, A(1, A(2, 2)));
+  A(1, A(1, A(2 - 1, A(2, 2 - 1))));
+  A(1, A(1, A(1, A(2, 1))));
+  A(1, A(1, A(1, 2)));
+  A(1, A(1, A(1 - 1, A(1, 2 - 1))));
+  A(1, A(1, A(0, A(1, 1))));
+  A(1, A(1, A(0, 2)));
+  A(1, A(1, 2 * 2));
+  A(1, A(1, 4));
+  A(1, A(1 - 1, A(1, 4 - 1)));
+  A(1, A(0, A(1, 3)));
+  A(1, A(0, A(1 - 1, A(1, 3 - 1))));
+  A(1, A(0, A(0, A(1, 2))));
+  A(1, A(0, A(0, A(1 - 1, A(1, 2 - 1)))));
+  A(1, A(0, A(0, A(0, A(1, 1)))));
+  A(1, A(0, A(0, A(0, 2))));
+  A(1, A(0, A(0, 2 * 2)));
+  A(1, A(0, 2 * 2 * 2));
+  A(1, 2 * 2 * 2 * 2);
+  A(1, 16);
+  // Based on the result of A(1, 10), we now know the result of A(1, 16) is 2^16.
+  // Many evaluation steps later...
+  // ...
+  // ...
+  65536;
+}
+
+// A(3, 3);
+
+if (false) {
+  A(3, 3);
+  A(3 - 1, A(3, 3 - 1));
+  A(2, A(3, 2));
+  A(2, A(3 - 1, A(3, 2 - 1)));
+  A(2, A(2, A(3, 1)));
+  A(2, A(2, 2));
+  A(2, A(2 - 1, A(2, 2 - 1)));
+  A(2, A(1, A(2, 1)));
+  A(2, A(1, 2));
+  A(2, A(1 - 1, A(1, 2 - 1)));
+  A(2, A(0, A(1, 1)));
+  A(2, A(0, 2));
+  A(2, 2 * 2);
+  A(2, 4);
+  // Based on the result of A(2, 4), we know the result here is 65536.
+  // Many evaluation steps later...
+  // ...
+  // ...
+  65536;
+}
+
+/**
+ * As a mathematical definition of this function, we can say that:
+ *
+ * f(n) computes 2n
+ */
+function f(n) {
+  return A(0, n);
+}
+
+/**
+ * As a mathematical definition of this function, we can say that:
+ *
+ * g(n) computes 2^n
+ */
+function g(n) {
+  return A(1, n);
+}
+
+/**
+ * As a mathematical definition of this function, we can say that:
+ *
+ * h(n) computes 2^(2^n)
+ *
+ * ^^ This isn't the right answer. I can't figure this one out.
+ */
+function h(n) {
+  return A(2, n);
 }
